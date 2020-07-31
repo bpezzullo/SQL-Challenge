c9c120c79b88248dcd89c6cd1cfc81aff39d72e5
Bill Pezzullo <bpezzullo@gmail.com>
2020-07-31 10:44:39 -0400

    Cleaned up schema and had to handle dollars

--- EmployeeSQL/Schema.sql
+++ EmployeeSQL/Schema.sql
@@ -1,98 +1,46 @@
-BEGIN
 
-
-CREATE TABLE titles (
+CREATE TABLE IF NOT EXISTS titles (
 	Employee_title Varchar(5) Primary Key,
 	Title varchar(25)
 );
 
-Create Table departments (
+Create Table IF NOT EXISTS departments (
 	Dept_no varchar(5) Primary Key,
 	Name varchar(25)
 );
 
-ALTER TABLE public.departments
-    ADD CONSTRAINT "Dept_no" FOREIGN KEY (dept_no)
-    REFERENCES public.dept_name (dept_no) MATCH SIMPLE
-    ON UPDATE RESTRICT
-    ON DELETE RESTRICT
-    DEFERRABLE INITIALLY DEFERRED
-    NOT VALID;
-
-CREATE TABLE employees (
+CREATE TABLE IF NOT EXISTS employees (
 	Employee_no INTEGER Primary Key,
 	Title varchar(5),
 	DOB date,
 	First_name varchar(25),
 	Last_name varchar(25),
 	Gender varchar(1),
-	Hire_date date
+	Hire_date date,
+	FOREIGN KEY (Title) REFERENCES titles(employee_title)
 );
-ALTER TABLE public.employee
-    ADD CONSTRAINT "employee_title" FOREIGN KEY (Title)
-    REFERENCES public.titles (employee_title) MATCH SIMPLE
-    ON UPDATE RESTRICT
-    ON DELETE RESTRICT
-    DEFERRABLE INITIALLY DEFERRED
-    NOT VALID;
 
-CREATE TABLE dept_emp (
+CREATE TABLE IF NOT EXISTS dept_emp (
 	Employee_no Integer,
-	Dept_no varchar(5)
-);
-ALTER TABLE public.dept_emp
-    ADD CONSTRAINT "Dept_no" FOREIGN KEY (dept_no)
-    REFERENCES public.departments (dept_no) MATCH SIMPLE
-    ON UPDATE RESTRICT
-    ON DELETE RESTRICT
-    DEFERRABLE INITIALLY DEFERRED
-    NOT VALID;
-
-ALTER TABLE public.dept_emp
-    ADD CONSTRAINT "employee_no" FOREIGN KEY (employee_no)
-    REFERENCES public.employees (employee_no) MATCH SIMPLE
-    ON UPDATE RESTRICT
-    ON DELETE RESTRICT
-    DEFERRABLE INITIALLY DEFERRED
-    NOT VALID;
-	
-CREATE TABLE dept_manager (
+	FOREIGN KEY (Employee_no) REFERENCES employees(Employee_no),
 	Dept_no varchar(5),
-	Employee_no INT
+	FOREIGN KEY (Dept_no) REFERENCES departments(Dept_no),
+	PRIMARY KEY (Employee_no, Dept_no)
 );
 	
-ALTER TABLE public.dept_manager
-    ADD CONSTRAINT "employee_no" FOREIGN KEY (employee_no)
-    REFERENCES public.employees (employee_no) MATCH SIMPLE
-    ON UPDATE RESTRICT
-    ON DELETE RESTRICT
-    DEFERRABLE INITIALLY DEFERRED
-    NOT VALID;
+CREATE TABLE IF NOT EXISTS dept_manager (
+	Dept_no varchar(5),
+	FOREIGN KEY (Dept_no) REFERENCES departments(Dept_no),
+	Employee_no INT,
+	FOREIGN KEY (Employee_no) REFERENCES employees(Employee_no),
+	PRIMARY KEY (Dept_no,Employee_no)
+);
 
-	
-ALTER TABLE public.dept_manager
-    ADD CONSTRAINT "Dept_no" FOREIGN KEY (Dept_no)
-    REFERENCES public.departments (Dept_no) MATCH SIMPLE
-    ON UPDATE RESTRICT
-    ON DELETE RESTRICT
-    DEFERRABLE INITIALLY DEFERRED
-    NOT VALID;
-	
-
-CREATE TABLE salaries (
+CREATE TABLE IF NOT EXISTS salaries (
 	Employee_no INT Primary Key,
+	FOREIGN KEY (Employee_no) REFERENCES employees(Employee_no),
 	Salary money
 );
 
-	
-ALTER TABLE public.salaries
-    ADD CONSTRAINT "employee_no" FOREIGN KEY (employee_no)
-    REFERENCES public.employees (employee_no) MATCH SIMPLE
-    ON UPDATE RESTRICT
-    ON DELETE RESTRICT
-    DEFERRABLE INITIALLY DEFERRED
-    NOT VALID;
 
 
-
-END;
